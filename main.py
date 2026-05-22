35ce681fbaede2dec0e8dbfce07257ac97b6ca8d
svsplayer <s_vs_player@yahoo.com>
2026-05-22 21:19:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ SCHEDULE = {
     # Pause 12:00 -> 13:00
     # Continues 13:00 -> 15:50
 
-    "09:00": "You Won 2 858 104.02 EUR at Loto 6/49",
+    "09:00": "You Won 2 935 712.23 EUR at Loto 6/49",
     "09:10": "You Won 2700 RON at Canada Atlantic Bucko lottery",
     "09:20": "You Won 1800 RON at SUA Florida Fantasy lottery",
     "09:30": "You Won 1150 RON at Italia Win for Life lottery",
@@ -41,7 +41,7 @@ SCHEDULE = {
     "09:50": "You Won 5500 RON at Ungaria Putto lottery",
 
     "10:00": "You Won 1200 RON at SUA Colorado Cash",
-    "10:10": "You Won 2 858 104.02 EUR at Loto 6/49",
+    "10:10": "You Won 2 935 712.23 EUR at Loto 6/49",
     "10:20": "You Won 2700 RON at Canada Atlantic Bucko lottery",
     "10:30": "You Won 1800 RON at SUA Florida Fantasy lottery",
     "10:40": "You Won 1150 RON at Italia Win for Life lottery",
@@ -49,7 +49,7 @@ SCHEDULE = {
 
     "11:00": "You Won 5500 RON at Ungaria Putto lottery",
     "11:10": "You Won 1200 RON at SUA Colorado Cash",
-    "11:20": "You Won 2 858 104.02 EUR at Loto 6/49",
+    "11:20": "You Won 2 935 712.23 EUR at Loto 6/49",
     "11:30": "You Won 2700 RON at Canada Atlantic Bucko lottery",
     "11:40": "You Won 1800 RON at SUA Florida Fantasy lottery",
     "11:50": "You Won 1150 RON at Italia Win for Life lottery",
@@ -59,7 +59,7 @@ SCHEDULE = {
     "13:00": "You Won 1140 RON at SUA Wisconsin Badger lottery",
     "13:10": "You Won 5500 RON at Ungaria Putto lottery",
     "13:20": "You Won 1200 RON at SUA Colorado Cash",
-    "13:30": "You Won 2 858 104.02 EUR at Loto 6/49",
+    "13:30": "You Won 2 935 712.23 EUR at Loto 6/49",
     "13:40": "You Won 2700 RON at Canada Atlantic Bucko lottery",
     "13:50": "You Won 1800 RON at SUA Florida Fantasy lottery",
 
@@ -67,7 +67,7 @@ SCHEDULE = {
     "14:10": "You Won 1140 RON at SUA Wisconsin Badger lottery",
     "14:20": "You Won 5500 RON at Ungaria Putto lottery",
     "14:30": "You Won 1200 RON at SUA Colorado Cash",
-    "14:40": "You Won 2 858 104.02 EUR at Loto 6/49",
+    "14:40": "You Won 2 935 712.23 EUR at Loto 6/49",
     "14:50": "You Won 2700 RON at Canada Atlantic Bucko lottery",
 
     "15:00": "You Won 1800 RON at SUA Florida Fantasy lottery",
@@ -75,7 +75,7 @@ SCHEDULE = {
     "15:20": "You Won 1140 RON at SUA Wisconsin Badger lottery",
     "15:30": "You Won 5500 RON at Ungaria Putto lottery",
     "15:40": "You Won 1200 RON at SUA Colorado Cash",
-    "15:50": "You Won 2 858 104.02 EUR at Loto 6/49",
+    "15:50": "You Won 2 935 712.23 EUR at Loto 6/49",
 
     # ===================== EVENING =====================
     # Runs from 20:00 -> 22:50
@@ -87,7 +87,7 @@ SCHEDULE = {
     "20:40": "You Won 5500 RON at Ungaria Putto lottery",
     "20:50": "You Won 1200 RON at SUA Colorado Cash",
 
-    "21:00": "You Won 2 858 104.02 EUR at Loto 6/49",
+    "21:00": "You Won 2 935 712.23 EUR at Loto 6/49",
     "21:10": "You Won 2700 RON at Canada Atlantic Bucko lottery",
     "21:20": "You Won 1800 RON at SUA Florida Fantasy lottery",
     "21:30": "You Won 1150 RON at Italia Win for Life lottery",
@@ -95,7 +95,7 @@ SCHEDULE = {
     "21:50": "You Won 5500 RON at Ungaria Putto lottery",
 
     "22:00": "You Won 1200 RON at SUA Colorado Cash",
-    "22:10": "You Won 2 858 104.02 EUR at Loto 6/49",
+    "22:10": "You Won 2 935 712.23 EUR at Loto 6/49",
     "22:20": "You Won 2700 RON at Canada Atlantic Bucko lottery",
     "22:30": "You Won 1800 RON at SUA Florida Fantasy lottery",
     "22:40": "You Won 1150 RON at Italia Win for Life lottery",
